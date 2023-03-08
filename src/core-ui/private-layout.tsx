64fbf8c47a8207a048f213f5396743d419f94446
Jonas Sørensen <jas@Jonass-MacBook-Pro.local>
2023-03-08 12:56:40 +0100

    GraphQL removed. Laravel API more integrated.

--- src/core-ui/private-layout.tsx
+++ src/core-ui/private-layout.tsx
@@ -3,17 +3,17 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
 // Internal
-import { useAPI } from '../hooks';
+import { laravelAPI } from '../hooks';
 import './template.min.css';
 
 export const PrivateLayout = ({ children } : any) => {
     const navigate = useNavigate();
+    const { getRequest } = laravelAPI()
 
     const [myMenuActive, setMyMenuActive] = useState(false)
     const toggleMyMenu = () => {
         setMyMenuActive(!myMenuActive)
     }
-    const { apiData, simpleAPIRequest, rawAPIRequest } = useAPI()
 
     const [wpBlogSettings, setWPBlogSettings] = useState<any>(null)
     const [myMenuItems, setMyMenuItems] = useState<any>(null)
@@ -24,36 +24,13 @@ export const PrivateLayout = ({ children } : any) => {
     }
 
     useEffect(() => {
-        let query1 = `query {
-            generalSettings {
-            title
-            description
-            }
-        }`;
-        let variables1 : any = null
-        rawAPIRequest(query1, variables1)
-            .then((res: any) => {
-                setWPBlogSettings(res.data)
-            });
+        getRequest("generalPageInfo").then(({ data }) => {
+            setWPBlogSettings(data)
+        })
 
-        let query2 = `query ($menuId: ID!) {
-            menu(id: $menuId, idType: DATABASE_ID) {
-            count
-            databaseId
-            slug
-            menuItems {
-                nodes {
-                url
-                label
-                }
-            }
-            }
-        }`;
-        let variables2 = { menuId: 174 };
-        rawAPIRequest(query2, variables2)
-            .then((res: any) => {
-                setMyMenuItems(res.data)
-            });
+        getRequest("getMenuLocation/174").then(({ data }) => {
+            setMyMenuItems(data)
+        })
     }, [])
 
     return (
@@ -61,10 +38,10 @@ export const PrivateLayout = ({ children } : any) => {
             <div id="top-header">
                 <Link to="/">
                     <span id="page-logo">
-                        <img alt={ wpBlogSettings && wpBlogSettings.data.generalSettings.title } src={require('../Assets/Images/headerLogo.png')} />
+                        <img alt={ wpBlogSettings && wpBlogSettings.blogname } src={require('../Assets/Images/headerLogo.png')} />
                     </span>
-                    <span id="logo-title">{ wpBlogSettings && wpBlogSettings.data.generalSettings.title }</span>
-                    <span id="logo-teaser">{ wpBlogSettings && wpBlogSettings.data.generalSettings.description }</span>
+                    <span id="logo-title">{ wpBlogSettings && wpBlogSettings.blogname }</span>
+                    <span id="logo-teaser">{ wpBlogSettings && wpBlogSettings.blogdescription }</span>
                 </Link>
 
                 <div id="right-nav">
@@ -76,10 +53,10 @@ export const PrivateLayout = ({ children } : any) => {
             </div>
             <div id="side-nav" className={(myMenuActive ? 'open-nav' : '')}>
                 {
-                    myMenuItems && myMenuItems.data.menu.menuItems.nodes.map((item: any, key: string) => {
+                    myMenuItems && myMenuItems.map((item: any, key: string) => {
                         return (
                             <div className="side-nav-item" key={key}>
-                                <span className="side-nav-item-link" onClick={() => sideNavBrowse(item.url)}>{item.label}</span>
+                                <span className="side-nav-item-link" onClick={() => sideNavBrowse(item.meta_value)}>{item.post_title}</span>
                             </div>
                         )
                     })
--- src/hooks/laravelAPI-axois.tsx
+++ src/hooks/laravelAPI-axois.tsx
@@ -5,17 +5,21 @@ import axios from 'axios'
 import { API_CONFIG } from './laravel-config'
 
 export const laravelAPI = () => {
-    const readAllOrders = (searchTerm : string) => {
+    const postWithString = (apiPoint : string, searchTerm : string = '') => {
         return axios
-            .post(`${API_CONFIG.BASE_URL}/readAllOrdersSummary`, 
+            .post(`${API_CONFIG.BASE_URL}/${apiPoint}`, 
             {
                 searchTerm: searchTerm
             })
-        /*return axios
-            .get(`${API_CONFIG.BASE_URL}/readAllOrdersSummary`)*/
+    }
+
+    const getRequest = (apiPoint : string) => {
+        return axios
+            .get(`${API_CONFIG.BASE_URL}/${apiPoint}`)
     }
 
     return {
-        readAllOrders
+        postWithString,
+        getRequest
     }
 }
